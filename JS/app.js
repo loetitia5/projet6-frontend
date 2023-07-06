fa98adfbed318e94df52f909a3354a7ed6863e81
loetitia <loetitia59@gmail.com>
2023-07-06 10:11:15 +0200

    gallery et login.js

--- JS/app.js
+++ JS/app.js
@@ -4,7 +4,11 @@ fetch ('http://localhost:5678/api/works')
 .then((works) => works.json())
 .then((data) => console.log(data));
 */
-const filters = document.getElementById("filters");
+//Récupérer les données de l'API "works"
+const filters = document.querySelectorAll(".filter");
+
+
+
 const galleryElement = document.querySelector(".gallery");
 const filterList = Array.from;
 
@@ -16,41 +20,64 @@ async function fetchWorks() {
 fetchWorks().then(works => {
    // Fonction d’affichage des filtres 
   function displayFilters(works) {
+    const filters = document.getElementById("filters");
     // Affichage des filtres
       if (displayFilters(works)) {       
         // Ajouter le filtre "Tous" en premier dans la liste des filtres
-        filters.innerHTML += `<li class="filter" id="0"></li>`;
+        filters.innerHTML += `<li class="filter" id="0">Tous</li>`;
+        //création d'une liste de filtres en triant les catégories
+        filterList = Array.from(works.mapFn(jsonWork =>jsonWork.categoryId));
+        console.log(filterList);
+
+        //trouve le premier element
+        filterList.forEach(categoryId => {
+        const categoryName = works.find(work => work.categoryId === categoryId).category.name;
+        filters.innerText = `<li class="filter" id="${categoryId}">${categoryName}</li>`;
+  })
       } 
       // Afficher les projets dans la galerie
       else (fetchWorksDisplayGallery(".gallery")); {
         // Fonction d’affichage de la galerie de la page d’accueil ou du mode
         function fetchWorksDisplayGallery(targetElement) {
-          return fetchWorks()
-            .then(works => { 
         // Sélectionnez l’élément de la galerie cible dans le sélecteur spécifié
           galleryElement.innerHTML = "";
-/*
-          works.forEach(jsonWork => {
-            // Create the figure element to represent the project
-            // Créer l’élément de figure pour représenter le projet
-            const figure = document.createElement('figure');
-            figure.classList.add('work');
-            figure.dataset.categories = jsonWork.category;
-          })*/
-        })
-            //création d'une liste de filtres en triant les catégories
-            filterList = Array.from(new set(works.map(jsonWork =>jsonWork.categoryId)));
-            console.log(filterList);
-  
-            filterList.forEach(categoryId => {
-              const categoryName = works.find(work => work.categoryId === categoryId).category.name;
-              filters.innerText = `<li class="filter" id="${categoryId}">${categoryName}</li>`;
-              })
-          }
-        }
-    }
-});
+          return works()
+          .then(works => {
+            const galleryElement = document.querySelector(targetElement);
 
+            works.forEach(jsonWork => {
+              const figure = document.createElement('figure');
+              figure.classLiss.add('work');
+              figure.dataset.category = jsonWork.categoryId;
+              
+              const img = document.createElement('img');
+              img.src = jsonWork.imageUrl;
+              img.alt = 'image projet';
+              
+              figure.appendChild(img);
+              if(targetElement === 'gallery') {
+                const figcaption = document.createElement('figcaption');
+                figcaption.textContent =jsonWork.title;
+                figure.appendChild(figcaption);
+              }
+              if(targetElement === 'modal-gallery') {
+                const figcaption = document.createElement('figcaption');
+                figcaption.textContent ='éditer';
+                figure.appendChild(figcaption);
+
+                const idWork = jsonWork.id;
+              }
+            
+          });
+        }) 
+        }     
+      }
+    }
+})
+
+  
+
+   
 //récupérer des données de l'API "catégories"
 /*
 fetch ('http://localhost:5678/api/categories')
@@ -64,26 +91,41 @@ async function fetchCategories() {
  }
 fetchCategories().then(categories => {
    
-  const buttons = document.querySelector("#filter");
+  const filter = document.querySelector("#filter");
 
   categories.forEach((category) => {
-    const button = document.createElement("li");
-    button.innerText = category.name;
-    button.classList.add("li");
+    const filter = document.createElement("li");
+   filter.innerText = category.name;
+    filter.classList.add("li");
 
-    button.addEventListener("click", async () => {
-      const getData = await fetchWorks();
+    filter.addEventListener("click", async () => {
+      const getData = await fetchWorks('http://localhost:5678/api/categories');
       const filterData = getData.filter(
         (data) => data.categoryId === category.id
       );
       renderGallery(filterData);
     });
     function addElement() {
-      const button = document.createElement('button');
-      button.classList.add('button_filter');
+      const filter = document.createElement('button');
+      filter.classList.add('button_filter');
   }
+//fonction pour créer un bouton, y appliquer une ou plusieurs classes et inserer un texte
+function createButtonElement(classNames = [], textContent = "") {
+  const button =document.createElement("div"); 
+  button.setAttribute("role", "button");
 
-    buttons.appendChild("ul");
+  if (Array.isArray(classNames)) {
+    classNames.forEach(className => {
+      button.classList.add(className);
+    });
+  } else {
+    button.classList.add(classNames);
+  }
+  if (textContent) {
+    button.textContent = textContent;
+  }
+  return button;
+}
   });
 });
   
---
+++ JS/login.js
@@ -0,0 +1,43 @@
+//page login 
+const header = document.querySelector("header");
+header.classList.add("header-logout");
+
+const form = document.getElementById("form");
+form.addEventListener("submit", submitForm); 
+
+
+//Gestion du formulaire
+
+
+async function submitForm(event){
+    event.preventDefault();
+
+    try {
+        const url ='http://localhost:5678/api/users/login';
+        const response =await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'        
+        },
+        body: JSON.stringity({
+            email: email.value, 
+            password: password.value
+        })
+    });
+
+    const responseData = await response.json();
+     
+    if (response === "error") {
+        const token = responseData.token; 
+
+        window.localStorage.setItem("token", token);
+        window.location.href = './index.htlm';
+    } else if (response === 'error') {
+     Message ("l'email ou mot de passe non valide") ; 
+    }  else if (response === 'error') {
+        Message ("l'utilisateur pas trouver");
+    } 
+    }catch (errur) {
+        Message ("erreur survenue lors de la connexion");
+    }
+}
